6f9bc9199b1dbd8cfe1ec3d4b3948bf9e9ded957
unknown <hellopratikraj@gmail.com>
2013-01-14 13:54:00 -0800

    added to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchEngine::Application.config.secret_token = 'c90bb2972c00564bd289d2d09d8b5ed0ce0670703ddce86abe477280e20c6f0a07d019dfd8afc838e9dbe250adb9208792ed97e2ca0af14e3d3be2a42e2a3790'
